139b2779dde6e6ffcd0be355c125e54399e2d888
danieloreniyi <danieloreniyi@gmail.com>
2025-11-01 07:12:30 -0700

    Added Header, MainContent, and Footer components and updated App.jsx

---
+++ alx-react-app/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+  return (
+    <footer>
+      <p>© 2025 My Favorite Cities. All rights reserved.</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ alx-react-app/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+function MainContent() {
+  return (
+    <main>
+      <ul>
+        <li>Lagos</li>
+        <li>Abuja</li>
+        <li>Port Harcourt</li>
+      </ul>
+    </main>
+  );
+}
+export default MainContent;
