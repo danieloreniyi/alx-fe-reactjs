0db8fe307a1f47c4507827a5f57f53cb36a4f214
danieloreniyi <danieloreniyi@gmail.com>
2025-11-01 07:16:14 -0700

    Fix: updated Footer text to match required content

--- alx-react-app/src/components/Footer.jsx
+++ alx-react-app/src/components/Footer.jsx
@@ -1,8 +1,9 @@
 function Footer() {
   return (
     <footer>
-      <p>© 2025 My Favorite Cities. All rights reserved.</p>
+      <p>© 2023 City Lovers</p>
     </footer>
   );
 }
+
 export default Footer;
