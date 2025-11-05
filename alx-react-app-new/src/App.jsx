db6399acdc7efb6e7518037f998218a9c969d32c
danieloreniyi <danieloreniyi@gmail.com>
2025-11-05 11:27:11 -0800

    Created a simple Counter app using useState hook

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,15 +1,10 @@
-import Header from './components/Header';
-import UserProfile from './components/UserProfile';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div>
-      <Header />
-      <MainContent />
-      <UserProfile name="John Doe" age={25} bio="Loves traveling and photography." />
-      <Footer />
+      <h1 style={{ textAlign: "center" }}>React Counter App</h1>
+      <Counter />
     </div>
   );
 }
