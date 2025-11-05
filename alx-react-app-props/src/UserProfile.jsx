2a961531ec5218df563ee2d2a7c1999c81e9285a
danieloreniyi <danieloreniyi@gmail.com>
2025-11-05 12:19:57 -0800

    Refactored UserProfile to use context directly

--- alx-react-app-props/src/UserProfile.jsx
+++ alx-react-app-props/src/UserProfile.jsx
@@ -1,7 +1,16 @@
-import ProfilePage from "./ProfilePage";
+import { useContext } from "react";
+import UserContext from "./UserContext";
 
 function UserProfile() {
-  return <ProfilePage />;
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
 }
 
 export default UserProfile;
