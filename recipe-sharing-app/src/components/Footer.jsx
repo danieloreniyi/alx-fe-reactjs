058ec5b4523e77f01072d8ce7d0e63ab3d3324a4
danieloreniyi <danieloreniyi@gmail.com>
2025-11-30 00:11:29 -0800

    Setup github-user-search project with Vite and React

--- recipe-sharing-app/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ backgroundColor: '#333', color: '#fff', textAlign: 'center', padding: '15px', marginTop: '20px' }}>
-      <p>© 2025 My Favorite Cities. All rights reserved.</p>
-    </footer>
-  );
-}
-
-export default Footer;
